8ada2b785477a7aed8f40f4a4b79e408430339fb
theamazingmrb <billie.heidelberg.jr@gmail.com>
2025-04-04 15:46:01 -0700

    Update database configuration for Heroku

--- babytracker/settings.py
+++ babytracker/settings.py
@@ -109,11 +109,18 @@ WSGI_APPLICATION = 'babytracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "babytracker_db",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'babytracker'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
+# For Heroku
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
